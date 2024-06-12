001e0043199d0965972285f39698a787234125e8
dylanonfb <87135939+dylanonfb@users.noreply.github.com>
2024-06-12 22:42:46 +0530

    Dockerfile added, authentication test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3
+FROM python:3 as build
 WORKDIR /app
 
 COPY ./requirement.txt ./requirement.txt
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+import secrets
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from typing import Annotated
+
 
 app = FastAPI()
 user = [
@@ -7,6 +12,8 @@ user = [
     {'id': 3, 'name': 'User3'},
     {'id': 4, 'name': 'User4'}
 ]
+
+creds={'username':'admin','password':'pass'}
 
 
 @app.get("/")
@@ -20,3 +27,26 @@ async def get_users():
 @app.get("/user/{id}")
 async def get_users(id:int):
     return [ out_user for out_user in user if out_user['id'] == id ]
+
+
+security = HTTPBasic()
+
+
+def user_prompt(credentials: Annotated[HTTPBasicCredentials,Depends(security)]):
+    in_user = credentials.username.encode('utf8')
+    in_pass = credentials.password.encode('utf8')
+
+    if secrets.compare_digest(in_user,creds['username'].encode('utf8')) \
+            and secrets.compare_digest(in_pass,creds['password'].encode('utf8')):
+        return in_user
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+
+@app.get("/login")
+async def login(user: Annotated[str, Depends(user_prompt)]):
+    return {'user':user}
